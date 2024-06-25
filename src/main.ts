4281f209e0787c5951500c91b33b0630b1a1ffb0
Svetoslav <svetoslav@alkem.io>
2024-06-25 11:38:16 +0300

    etag plugin & config

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,42 @@
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { NestFactory } from '@nestjs/core';
-// import {
-//   FastifyAdapter,
-//   NestFastifyApplication,
-// } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
-// import helmet from '@fastify/helmet';
-// import fastifyCookie from '@fastify/cookie';
-// import cors from '@fastify/cors';
-import * as cookieParser from 'cookie-parser';
+import helmet from '@fastify/helmet';
+import fastifyCookie from '@fastify/cookie';
+import cors from '@fastify/cors';
+import etag from '@fastify/etag';
 import { AppModule } from './app.module';
 import { ConfigType } from './config';
-import helmet from 'helmet';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 const isProd = process.env.NODE_ENV === 'production';
 
 (async () => {
-  const app =
-    await NestFactory.create<NestExpressApplication /*NestFastifyApplication*/>(
-      AppModule,
-      // new FastifyAdapter({ logger: !isProd }),
-      {
-        /***
-         * if the logger is provided at a later stage via 'useLogger' after the app has initialized, Nest falls back to the default logger
-         * while initializing, which logs a lot of info logs, which we don't have control over and don't want tracked.
-         * The logger is disabled while the app is loading ONLY on production to avoid the messages;
-         * then the costume logger is applied as usual
-         */
-        logger: isProd ? false : undefined,
-      },
-    );
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: !isProd }),
+    {
+      /***
+       * if the logger is provided at a later stage via 'useLogger' after the app has initialized, Nest falls back to the default logger
+       * while initializing, which logs a lot of info logs, which we don't have control over and don't want tracked.
+       * The logger is disabled while the app is loading ONLY on production to avoid the messages;
+       * then the costume logger is applied as usual
+       */
+      logger: isProd ? false : undefined,
+    },
+  );
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
 
-  // await app.register(fastifyCookie);
-  // app.use(
-  //   helmet({
-  //     contentSecurityPolicy: false,
-  //   }),
-  // );
-  // await app.register(helmet, { contentSecurityPolicy: false });
-  // await app.register(cors, { origin: false });
-  app.use(cookieParser());
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-    }),
-  );
+  await app.register(fastifyCookie);
+  await app.register(helmet, { contentSecurityPolicy: false });
+  await app.register(cors, { origin: false });
+  // set the header manually, because this plugin skips generation for Stream (our case)
+  // it only supports String and Buffer
+  await app.register(etag);
 
   const configService: ConfigService<ConfigType, true> = app.get(ConfigService);
   const port = configService.get('settings.application.port', { infer: true });
--- src/services/file-reader/file.controller.ts
+++ src/services/file-reader/file.controller.ts
@@ -12,27 +12,34 @@ import {
   Res,
   StreamableFile,
 } from '@nestjs/common';
-import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-// import { FastifyReply } from 'fastify';
+import { FastifyReply } from 'fastify';
 import { FileService } from './file.service';
 import { DocumentData, FileInfoErrorCode } from './types';
 import { FileInfoException } from './exceptions';
+import { ConfigType } from '../../config';
 
 @Controller('/rest/storage')
 export class FileController {
+  private readonly documentMaxAge: number;
   constructor(
     private readonly fileService: FileService,
+    private readonly configService: ConfigService<ConfigType, true>,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: LoggerService,
-  ) {}
+  ) {
+    this.documentMaxAge = this.configService.get(
+      'settings.application.document_max_age',
+      { infer: true },
+    );
+  }
 
   @Get('document/:id')
   public async file(
     @Param('id') id: string,
     @Headers('authorization') authorization: string | undefined,
     @Headers('cookie') cookie: string | undefined,
-    // @Res({ passthrough: true }) res: FastifyReply,
-    @Res({ passthrough: true }) res: Response,
+    @Res({ passthrough: true }) res: FastifyReply,
   ): Promise<StreamableFile> {
     let documentData: DocumentData | undefined;
 
@@ -46,22 +53,13 @@ export class FileController {
       throw handleReadErrorByCode(e);
     }
 
-    // res.headers({
-    //   'Content-Type': `${documentData.mimeType}`,
-    //   'Cache-Control': 'public, max-age=15552000',
-    //   Pragma: 'public',
-    //   Expires: new Date(Date.now() + 15552000 * 1000).toUTCString(),
-    // });
-    res.setHeader('Content-Type', `${documentData.mimeType}`);
-
-    // Set caching headers
-    res.setHeader('Cache-Control', 'public, max-age=15552000');
-    res.setHeader('Pragma', 'public');
-    res.setHeader(
-      'Expires',
-      new Date(Date.now() + 15552000 * 1000).toUTCString(),
-    );
-
+    res.headers({
+      'Content-Type': `${documentData.mimeType}`,
+      'Cache-Control': `public, max-age=${this.documentMaxAge}`,
+      Pragma: 'public',
+      Expires: new Date(Date.now() + this.documentMaxAge * 1000).toUTCString(),
+      etag: id,
+    });
     this.logger.verbose?.(`Serving document ${id}`);
 
     return documentData.file;
